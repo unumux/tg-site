916a380f2975ae7dc07238be4950243f3a16dc36
Kyle Wagner <Keale2@gmail.com>
2017-10-05 15:55:40 -0400

    Updates Themes component

--- src/components/Themes.js
+++ src/components/Themes.js
@@ -1,14 +1,51 @@
 import React from 'react';
 import styled from 'styled-components';
+import { Swatch } from './Swatch';
+
 
 const StyledThemes = styled.div`
     color: red;
 `;
 
+const Heading = styled.h1`
+
+`;
+
+const ThemeSection = styled.section`
+
+`;
+
+const SectionHeading = styled.h1`
+
+`;
+
+const Swatches = styled.ul`
+
+`;
+
+const StyledSwatch = styled(Swatch)`
+
+`;
+
 export const Themes = props =>
     <StyledThemes>
-        <h1>theme-variable-whatever</h1>
-        <p>Hex Value</p>
+        <Heading>theme-variable-whatever</Heading>
+        <ThemeSection>
+            <SectionHeading>Primary Colors</SectionHeading>
+            <Swatches>
+                <StyledSwatch name="Purple" hex="#f0f" />
+                <StyledSwatch name="Purple" hex="#f0f" />
+                <StyledSwatch name="Purple" hex="#f0f" />
+            </Swatches>
+        </ThemeSection>
+        <ThemeSection>
+            <SectionHeading>Accent Colors</SectionHeading>
+            <Swatches>
+                <StyledSwatch name="Purple" hex="#f0f" />
+                <StyledSwatch name="Purple" hex="#f0f" />
+                <StyledSwatch name="Purple" hex="#f0f" />
+            </Swatches>
+        </ThemeSection>
     </StyledThemes>
 ;
 
