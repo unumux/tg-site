0dfafe3cca8c2032de1901221d069b96ef0937de
Sara Kennedy <slkennedy@gmail.com>
2017-10-05 15:07:23 -0400

    starts on theme-palette

--- src/components/Themes.js
+++ src/components/Themes.js
@@ -1,3 +1,14 @@
 import React from 'react';
+import styled from 'styled-components';
 
-export const Themes = (props) => <p>Themes</p>;
+const ThemePalette = styled.div;
+
+export const Themes = (props) => 
+    <ThemePalette>
+        <div> 
+            <div></div>
+            <h1>$theme-variable-whatever</h1>
+            <p>Hex Value</p>
+        </div>
+    </ThemePalette>
+;
