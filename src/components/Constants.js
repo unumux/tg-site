2eae578018a5688ff9e629ab3863aa0019534f52
Bolling <bball@unum.com>
2017-10-05 16:47:32 -0400

    updates Constant swatches spacing

--- src/components/Constants.js
+++ src/components/Constants.js
@@ -1,19 +1,25 @@
 import React from 'react';
 import styled from 'styled-components';
 import { Swatch } from './Swatch';
+import { Container } from './Container';
 
 const StyledConstants = styled.section`
-    
+    background-color: #ddd;
 `;
 
 const Heading = styled.h1`
 
 `;
 
+const StyledSwatch = styled(Swatch)`
+    margin-right: 2rem;
+`;
+
 const Swatches = styled.div`
     display: flex;
-    justify-content: space-around;
+    justify-content: flex-start;
 `;
+
 
 const Add = styled.a`
     background-color: #0ca;
@@ -31,11 +37,13 @@ const Add = styled.a`
 
 export const Constants = props => 
     <StyledConstants>
-        <Heading>Constants</Heading>
+        <Container>
+            <Heading>Constants</Heading>
             <Swatches>
-                <Swatch name='Purple' hex='#127881' />
-                <Swatch name='Blue' hex='#669933' />
-                <Swatch name='Yellowwww' hex='#ff7700' />
+                <StyledSwatch name='Purple' hex='#127881' />
+                <StyledSwatch name='Blue' hex='#669933' />
+                <StyledSwatch name='Yellowwww' hex='#ff7700' />
             </Swatches>
             <Add>Add</Add>
+        </Container>
     </StyledConstants>
